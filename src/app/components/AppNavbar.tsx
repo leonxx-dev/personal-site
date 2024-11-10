8cc1e19ad5306b410f067c9c79d323d3448c4fa4
Jevgeni Glazunov <jevgeni.glazunov@gmail.com>
2024-11-10 15:43:23 +0100

    Setup and add pages

---
+++ src/app/components/AppNavbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function AppNavbar () {
+  const pathname = usePathname();
+
+  const navLinks = [
+    {
+      href: "/",
+      name: "Home",
+    },
+    {
+      href: "/portfolio",
+      name: "Portfolio"
+    },
+    {
+      href: "/about",
+      name: "About"
+    },
+    {
+      href: "/blog",
+      name: "Blog"
+    }
+  ];
+
+  return (
+    <nav className="flex justify-between mx-auto w-full max-w-screen-xl py-4">
+      <div>
+        <Link href="/">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Brand logo"
+            width={120}
+            height={30}
+            priority
+          />
+        </Link>
+      </div>
+      <div className="flex gap-8">
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href
+          return <Link href={link.href} key={link.name} className={isActive ? "text-red-500" : ""}>{link.name}</Link>
+        })}
+      </div>
+    </nav>
+  );
+}
