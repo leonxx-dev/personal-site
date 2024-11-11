9bc493b1733d31bdc62f60f62971f4b1e9af656e
Leon2ev <jevgeni.glazunov@gmail.com>
2024-11-11 11:11:56 +0100

    Make navbar responsible

--- src/app/components/AppNavbar.tsx
+++ src/app/components/AppNavbar.tsx
@@ -3,8 +3,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function AppNavbar () {
+  const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
   const navLinks = [
@@ -27,25 +29,78 @@ export default function AppNavbar () {
   ];
 
   return (
-    <nav className="flex justify-between mx-auto w-full max-w-screen-xl py-4">
-      <div>
-        <Link href="/">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Brand logo"
-            width={120}
-            height={30}
-            priority
-          />
-        </Link>
-      </div>
-      <div className="flex gap-8">
-        {navLinks.map((link) => {
-          const isActive = pathname === link.href
-          return <Link href={link.href} key={link.name} className={isActive ? "text-red-500" : ""}>{link.name}</Link>
-        })}
-      </div>
-    </nav>
+    <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <Image 
+              className="h-8 w-auto"
+              src="/next.svg"
+              alt="Brand Logo"
+              width={120}
+              height={30}
+              priority
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={() => setOpen(true)}>
+            <span className="sr-only">Open main menu</span>
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navLinks.map((link) => {
+            const isActive = pathname === link.href
+            return <Link href={link.href} key={link.name} className={`text-sm/6 font-semibold text-gray-900 ${isActive ? "text-red-500" : ""}`}>{link.name}</Link>
+          })}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+      {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+      {open && <div className="lg:hidden" role="dialog" aria-modal="true">
+        {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
+        <div className="fixed inset-0 z-10"></div>
+        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <Image 
+                className="h-8 w-auto"
+                src="/next.svg"
+                alt="Brand Logo"
+                width={120}
+                height={30}
+                priority
+              />
+            </a>
+            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={() => setOpen(false)}>
+              <span className="sr-only">Close menu</span>
+              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navLinks.map((link) => {
+                  const isActive = pathname === link.href
+                  return <Link href={link.href} key={link.name} className={`-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 ${isActive ? "text-red-500" : ""}`}>{link.name}</Link>
+                })}
+              </div>
+              <div className="py-6">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>}
+    </header>
   );
 }
