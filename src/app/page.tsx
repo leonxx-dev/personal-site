8e9f40b086c3cc53e6ece00ec1d9a0b0a3c0e6c8
Leon2ev <jevgeni.glazunov@gmail.com>
2024-11-11 20:21:03 +0100

    Type fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col space-y-8 max-w-7xl items-center p-4 my-auto lg:flex-row lg:mx-auto lg:gap-4">
       <div  className="lg:basis-1/2 self-center space-y-8">
-        <Title>I’m here to make it happen.</Title>
+        <Title>I'm here to make it happen.</Title>
         <SubTitle>
           <span className="text-yellow-500">I help businesses</span> make a mark in the digital world and streamline their operations with smart software solutions.
           If you're ready <span className="text-yellow-500">to get noticed online</span> or want to take the busywork out of your day-to-day life.
