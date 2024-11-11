ee5dde57239a00ae2b5088adf5d3d6913506ed9a
Leon2ev <jevgeni.glazunov@gmail.com>
2024-11-11 20:32:15 +0100

    Fix quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="flex flex-col space-y-8 max-w-7xl items-center p-4 my-auto lg:flex-row lg:mx-auto lg:gap-4">
       <div  className="lg:basis-1/2 self-center space-y-8">
-        <Title>I am here to make it happen.</Title>
+        <Title>I&apos;m here to make it happen.</Title>
         <SubTitle>
           <span className="text-yellow-500">I help businesses</span> make a mark in the digital world and streamline their operations with smart software solutions.
-          If you're ready <span className="text-yellow-500">to get noticed online</span> or want to take the busywork out of your day-to-day life.
+          &apos;re ready <span className="text-yellow-500">to get noticed online</span> or want to take the busywork out of your day-to-day life.
         </SubTitle>
         <div className="mt-8">
         <Link href="/about">
