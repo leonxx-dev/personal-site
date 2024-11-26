4f9dd16dc35a74b3291f8b5b8a1f06cc3ec74511
Leon2ev <jevgeni.glazunov@gmail.com>
2024-11-26 11:57:04 +0100

    Add achivements

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -15,19 +15,45 @@ export default function AboutPage() {
   return (
     <main className="flex flex-col space-y-8 max-w-4xl items-center p-4 my-auto lg:mx-auto lg:gap-4">
       <section className="my-10">
-        <div className="flex gap-8 items-center">
+        <div className="flex flex-col md:flex-row gap-8 items-center">
           <div className="flex-shrink-0">
             <Image width={500} height={500} src="/jevgeniGlazunov.webp" alt="Jevgeni Glazunov" className="w-48 h-48 rounded-full shadow-md border-2 border-gray-300" />
           </div>
           <div>
-          <h2 className="mb-4">Hello!</h2>
+            <h1 className="mb-4">About Me</h1>
             <p className="leading-relaxed">
-              I&apos;m a passionate software developer with a deep love for technology and problem-solving.
+              I&apos;m Jevgeni Glazunov, a passionate software developer with a deep love for technology and problem-solving.
+              Based in Oslo, Norway, I thrive on turning ideas into working solutions, bringing concepts to life through clean, efficient, and user-friendly software.
               My journey in the tech world has been fueled by curiosity, creativity, and a constant drive to learn and grow.
-              I thrive on turning ideas into working solutions, bringing concepts to life through clean, efficient, and user-friendly software.
             </p>
           </div>
         </div>
+      </section>
+      <section className="my-10">
+        <h2 className="mb-4">Achievements</h2>
+        <p className="leading-relaxed mb-4">A showcase of my accomplishments reflecting dedication, innovation, and impact.</p>
+        <ul className="list-disc ml-4 leading-relaxed">
+          <li>
+            <strong>Achieved First-Class Honors:</strong> Graduated top of the class from a coding bootcamp, excelling despite no prior
+            programming experience.
+          </li>
+          <li>
+            <strong>Solo Developer Success:</strong> Designed and implemented an admin application single-handedly, streamlining onboarding
+            and customer management processes for the company.
+          </li>
+          <li>
+            <strong>Educational Impact:</strong> Contributed as a key team member to the development of a kindergarten-focused educational platform,
+            enabling teachers to effectively plan activities and evaluate children performance.
+          </li>
+          <li>
+            <strong>Leadership in Legacy Modernization:</strong> Leading a team of four to revamp a 20-year-old insurance system, including
+            complete backend integration, data migration, and UI/UX transformation.
+          </li>
+          <li>
+            <strong>Hackathon Event Organizer:</strong> Contributed to the organization of NTNU University&apos;s 5th Annual BRAIN Hackathon,
+            collaborating with a team to plan technical aspects, manage participant communication, and ensure a seamless event experience.
+          </li>
+        </ul>
       </section>
       <section className="my-10">
         <h2 className="mb-4">My Expertise</h2>
@@ -36,23 +62,23 @@ export default function AboutPage() {
         </p>
         <ul className="list-disc ml-4 leading-relaxed">
           <li>
-            <span className="font-semibold">Frontend Development:</span> Using tools like React.js, Vue.js, Next.js,
+            <strong>Frontend Development:</strong> Using tools like React.js, Vue.js, Next.js,
             and TailwindCSS to create user-friendly, responsive, and visually appealing websites.
           </li>
           <li>
-            <span className="font-semibold">Backend Development:</span> Working with Node.js and TypeScript to build
+            <strong>Backend Development:</strong> Working with Node.js and TypeScript to build
             reliable and scalable server-side applications and APIs.
           </li>
           <li>
-            <span className="font-semibold">Database Management:</span> Skilled in working with SQL databases to handle
+            <strong>Database Management:</strong> Skilled in working with SQL databases to handle
             data smoothly and run queries efficiently.
           </li>
           <li>
-            <span className="font-semibold">System Integrations:</span> Connecting different systems to ensure they
+            <strong>System Integrations:</strong> Connecting different systems to ensure they
             work together seamlessly.
           </li>
           <li>
-            <span className="font-semibold">End-to-End Development:</span> Managing the entire development process,
+            <strong>End-to-End Development:</strong> Managing the entire development process,
             from idea to finished product, by breaking big projects into smaller, manageable tasks to deliver effective solutions on time.
           </li>
         </ul>
@@ -66,13 +92,13 @@ export default function AboutPage() {
         </p>
         <ul className="list-disc ml-4 leading-relaxed">
           <li>
-            <span className="font-semibold">From Idea to Reality:</span> Turning big ideas into working, practical applications.
+            <strong>From Idea to Reality:</strong> Turning big ideas into working, practical applications.
           </li>
           <li>
-            <span className="font-semibold">Scalable Solutions:</span> Creating systems, APIs, and interfaces that can grow and adapt over time.
+            <strong>Scalable Solutions:</strong> Creating systems, APIs, and interfaces that can grow and adapt over time.
           </li>
           <li>
-            <span className="font-semibold">Effective Problem-Solving:</span> Breaking down tough challenges into simple, workable solutions.
+            <strong>Effective Problem-Solving:</strong> Breaking down tough challenges into simple, workable solutions.
           </li>
         </ul>
       </section>
